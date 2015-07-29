89953566458deaa586dbf05d1ca05c716dc281cc
zannah m <zannah@Zannahs-MBP.home>
2015-07-29 17:35:11 -0400

    remove y for now by for now

--- project2/circle.js
+++ project2/circle.js
@@ -15,13 +15,13 @@ function Circle(x,y, size, xsp, ysp, thecolor)
   
   this.move = function()
   {
-    y+=ysp;
+   // y+=ysp;
     x+=xsp;
     
-    if(y>height-100 || y<200)
-     {
-       ysp= ysp*-1;
-     }
+    // if(y>height-100 || y<200)
+    // {
+    //   ysp= ysp*-1;
+    // }
     
     if(x>width || x<0)
     {
@@ -33,13 +33,13 @@ function Circle(x,y, size, xsp, ysp, thecolor)
   {
      y+=scrollspeed;
      this.move();
-     print("hello up");
+    // print("hello up");
   }
   
   this.downScroll = function()
   {
      y-=scrollspeed;
      this.move();
-     print("hello down");
+     //print("hello down");
   }
 }
