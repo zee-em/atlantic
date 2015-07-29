4bd4711b75d061c11573bd49aa2b03af25a46f5d
zannah m <zannah@Zannahs-MBP.home>
2015-07-29 19:07:47 -0400

    added zones

--- project2/sketch.js
+++ project2/sketch.js
@@ -8,6 +8,7 @@ var bluecircles = [];
 var greencircles = [];
 var count =150;
 var scrollspeed = 5;
+var zones = [];
 
 function setup() {
    createCanvas(800,600);
@@ -15,6 +16,7 @@ function setup() {
    pink = color(255,0,255);
    marineblue =color(0,200,255);
    seagreen= color(150,255,220);
+   
    for(var i =0; i<count; i++)
    {
      var c = new Circle(random(1,width),random(200,300),10,random(1,7),random(.005,1), pink, 150,300);
@@ -26,19 +28,28 @@ function setup() {
      var g = new Circle(random(1,width),random(400,600),20,random(.05,3),random(.005,.5),seagreen,400,600 );
      append(greencircles,g);
    }
+   
 
+   var zonePink = new Zone(150,300,circles);
+   var zoneBlue = new Zone(275,425,bluecircles);
+   var zoneGreen = new Zone(400,600,greencircles);
+   append(zones,zonePink);
+   append(zones,zoneBlue);
+   append(zones,zoneGreen);
 }
 
 function draw() {
   background(0);
-  for(var i =0; i<count; i++)
+  for(var i =0; i<zones.length; i++)
    {
-     circles[i].show();
-     circles[i].move();
-     bluecircles[i].show();
-     bluecircles[i].move();
-     greencircles[i].show();
-     greencircles[i].move();
+     if(zones[i].testToDisplay === true)
+     {
+       for(var j = 0; j<zones[i].length; j++)
+       {
+         zones[i].inhabitants[j].show();
+         zones[i].inhabitants[j].move();
+       }
+     }
    }
   fill(255);
   ellipse(100,100,25,25);
@@ -46,25 +57,23 @@ function draw() {
   if(overCircle(100,100,25) === true && mouseIsPressed)
   {
     //do something: add
-    for(var i =0; i<count; i++)
+   for(var i =0; i<zones.length; i++)
     {
-      circles[i].upScroll();
-      bluecircles[i].upScroll();
-      greencircles[i].upScroll();
+      for(var j = 0; j<zones[i].length; j++)
+       {
+         zones[i].inhabitants[j].upScroll();
+       }
     }
   }
    if(overCircle(100,300,25) === true && mouseIsPressed)
   {
     //do something: subtract
-    for(var i =0; i<count; i++)
+    for(var i =0; i<zones.length; i++)
     {
-      circles[i].downScroll();
-      bluecircles[i].downScroll();
-      greencircles[i].downScroll();
-      
-    }
-
-    
+      for(var j = 0; j<zones[i].length; j++)
+       {
+         zones[i].inhabitants[j].downScroll();
+       }
   }
 }
 
---
+++ project2/zone.js
@@ -0,0 +1,29 @@
+function Zone(ymin, ymax, inhabitants)
+{
+  this.inhabitants = inhabitants;
+  this.ymin = ymin;
+  this.ymax = ymax;
+  
+  this.testToDisplay = function()
+  {
+    if(ymin>height || ymax <0)
+    {
+      return false;
+    }
+    
+  }
+  
+  this.upZone = function()
+  {
+     ymax+=scrollspeed;
+     ymin+=scrollspeed;
+    // print("hello up");
+  }
+  
+  this.downZone = function()
+  {
+     ymax-=scrollspeed;
+     ymin-=scrollspeed;
+     //print("hello down");
+  }
+}
