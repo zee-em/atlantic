94df58c00e277094ff6bf2b6a7b9aa6bb695b900
zannah m <zannah@Zannahs-MBP.home>
2015-07-31 17:57:57 -0400

    changed print statment

--- project1/sketch.js
+++ project1/sketch.js
@@ -188,7 +188,7 @@ function loadZoneDataPts()
     var thisPart = new Part(partsList[i],i*100-25,(i*100-25)+100,10,.25,5,cl);
     partsData[partsList[i]] = thisPart;
   }
-  print(partsData[partsList["xx"]]);
+  print(partsData["xx"]);
 }
 
 function makeWords()
